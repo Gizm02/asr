15a74f6218c722782e79752d418566bbfbfd1d0c
Miguel Graça <miguelrgraca@gmail.com>
2014-11-11 19:59:22 +0100

    File reading done

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
 #include "Context.h"
 #include "SimpleOptimizer.h"
 #include "Solution.h"
@@ -10,6 +13,15 @@ int main()
 {
 
     Context<SimpleOptimizer> context();
+    ifstream file("material/u3/probe1.ascii.txt");
+
+    string line;
+    vector<double> energies;
+    while(getline(file,line)){
+        energies.push_back(strtod(line.c_str(),NULL));
+    }
+
+
 /********************************************//**
  *  Read in data from probe1.ascii here, initialize needed objects etc.
  ***********************************************/
