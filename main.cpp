7a8cb6fa0615600a2204a51cb9e7bd070fa86566
Miguel Graça <miguelrgraca@gmail.com>
2014-11-11 21:32:54 +0100

    derbe

--- main.cpp
+++ main.cpp
@@ -8,12 +8,22 @@
 #include "SimpleOptimizer.h"
 #include "Solution.h"
 
+#define K 3
+
 using namespace std;
-
-
+double h(double x,double x_mean) {
+    return ((pow((x-x_mean),2)));
+}
+double getMean(vector<double>& energies, size_t i, size_t j){
+    double mean=0;
+    for(int k=0;k<=(j-i);k++) {
+        mean+=energies.at(i*K+k);
+    }
+    mean*=1.0/(j-i);
+    return mean;
+}
 int main()
 {
-
     ifstream file("material/u3/probe1.ascii.txt");
 
     if(!file) { exit(EXIT_FAILURE);}
@@ -22,18 +32,39 @@ int main()
     while(getline(file,line)){
         energies.push_back(strtod(line.c_str(),NULL));
     }
-    Context<SimpleOptimizer> context;
-    context.setEnergies(energies);
     /*
     context.setEnergies(energies);
     vector<double>& test = context.getEnergies();
     cout << test.first() << endl;
     */
 
-
-/********************************************//**
- *  Read in data from probe1.ascii here, initialize needed objects etc.
- ***********************************************/
+    ///Test if the vectors are copied correctly
+    double mean=0;
+    size_t T = energies.size();
+    ///Set up the cost matrix
+    for(int i=0;i<(T-2);i++) {
+        for(int j=i+1;j<(T-1);j++) {
+            ///Determine the mean for the intervall [t_i,t_j]
+            mean=getMean(energies,i,j);
+            #if DEBUG>0
+            assert(mean>0);
+            #endif
+            double localCost=0;
+            for(int k=0;k<(j-i);k++) {
+                localCost+=context.h(energies.at(i+k),mean);
+            }
+            costs.at(i*K+j)=localCost;
+        }
+    }
+    double globalCosts;/**< Stores the global cost for a segmentation. */
+    double optimalCosts;/**< Stores the optimal cost found during iteration. */
+    for(int i=0;i<(T-2);i++) {
+        for(int j=i+1;j<(T-1);j++) {
+            globalCosts=costs.at(i)+costs.at(i*K+j)+costs.at(j*K+K);
+            optimalCosts=((globalCosts<optimalCosts)||i==0)?globalCosts:optimalCosts;
+        }
+    }
+    solution.setSolution(optimalCosts);/**< Store the result in the solution obj. */
 
     return 0;
 }
--- src/Solution.cpp
+++ src/Solution.cpp
@@ -1,12 +1,1 @@
-#include "Solution.h"
 
-Solution::Solution(double solution): solution(solution),multCount(0) { } //C++11 element initialize list
-
-Solution::~Solution()
-{
-    //dtor
-}
-
-void setSolution(double solution) {
-    solution=solution;
-}
