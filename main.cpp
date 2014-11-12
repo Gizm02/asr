54e1114b115006c7c584d84d935389790e82ca9d
Miguel Graça <miguelrgraca@gmail.com>
2014-11-12 02:50:27 +0100

    diagonal matrix gemacht

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include "Context.h"
 #include "SimpleOptimizer.h"
 #include "Solution.h"
-#define DEBUG 0
+#define DEBUG 1
 #define K 3
 
 using namespace std;
@@ -20,7 +20,7 @@ double getMean(vector<double>& energies, size_t i, size_t j){
     for(int k=0;k<=(j-i);k++) {
         mean+=energies.at(i+k);
     }
-    mean*=1.0/(j-i);
+    mean*=1.0/(j-i+1);
     return mean;
 }
 int main()
@@ -52,25 +52,18 @@ int main()
     size_t T = energies.size();
     ///Set up the cost matrix
     vector<double> costs(T*T);
-    for(int i=0;i<(T-2);i++) {
-        for(int j=i+1;j<(T-1);j++) {
+    for(int i=0;i<T;i++) {
+        cout << "HALLO" << endl;
+        for(int j=i;j<T;j++) {
             ///Determine the mean for the intervall [t_i,t_j]
-            mean=getMean(energies,i,j);
-            /*#if DEBUG>0
-            assert(mean!=0);
-            #endif*/
-            double localCost=0;
-            for(int k=0;k<(j-i);k++) {
-                localCost+=h(energies.at(i+k),mean);
-                //cout<<localCost<<" ";
-            }
-        //    cout<<endl;
-            costs.at(i*K+j)=localCost;
+            mean = getMean(energies,i,j);
+            costs.at(i*T+j) = h(energies.at(i),mean);
+            cout << mean << " " << costs.at(i*T+j) << endl;
         }
     }
     #if DEBUG>0
     for(const auto &element: costs) {
-        cout<<element<<" ";
+       // cout<<element<<" ";
     }
     #endif // DEBUG
     double globalCosts;/**< Stores the global cost for a segmentation. */
@@ -80,7 +73,7 @@ int main()
     optimalIndexes.at(K)=T;
     for(int i=0;i<(T-2);i++) {
         for(int j=i+1;j<(T-1);j++) {
-            globalCosts=costs.at(i)+costs.at(i*K+j)+costs.at(j*K+K);
+            globalCosts=costs.at(i)+costs.at(i*T+j)+costs.at(j*T+T);
             if ( globalCosts < optimalCosts){
                     cout<<"New minimal costs are:"<<globalCosts<<endl;
                 //optimalCosts = globalCosts;
@@ -92,5 +85,14 @@ int main()
     }
 
     cout << "Optimal Costs: "<<optimalCosts << " Optimal Indeces i and j: "<< optimalIndexes.at(1) << " " << optimalIndexes.at(2) << endl;
+    for (int i = 0; i < costs.size() ; i++){
+        for (int n = 1; n < 6; n++){
+            if (i == (n * 5)){
+                cout << endl;
+                break;
+            }
+        }
+        cout << *(costs.begin()+i)<< " " ;
+    }
     return 0;
 }
