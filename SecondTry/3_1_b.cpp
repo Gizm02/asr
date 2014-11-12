c0b3d702b11e816c54bd13eb8bf20df7ff24f6a2
Gizm02 <gizm0@gmx.net>
2014-11-12 04:47:22 +0100

    Super.

--- SecondTry/3_1_b.cpp
+++ SecondTry/3_1_b.cpp
@@ -21,7 +21,7 @@ double getMean(vector<double>& energies, unsigned int i, unsigned int j){
     for(int k=0;k<=(j-i);k++) {
         mean+=energies[i+k];
     }
-    mean=mean/(j-i+1);
+    mean/=j-i;
     return mean;
 }
 
