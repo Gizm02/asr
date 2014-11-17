c985c38eff5bd353a384a1ef89019c180841e0f8
Miguel Graça <miguelrgraca@gmail.com>
2014-11-17 16:33:39 +0100

    new main, has to be rewritten

--- u4_main.cpp
+++ u4_main.cpp
@@ -7,7 +7,7 @@
 int main(){
 	string hyp_name = "materials/u4/test.dat";
 	string ref_name = "materials/u4/ref.dat";
-	vector<double> weights = {2,0,2};
+	vector<double> weights = {2.0,0.0,2.0};
 
 	TimeAlignment t(hyp_name, ref_name, weights);
  
