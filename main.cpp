f714bc3f0d112ef30647eabcec504fdb8198f4b6
Miguel Graça <miguelrgraca@gmail.com>
2014-11-17 16:50:55 +0100

    Main switch

--- main.cpp
+++ main.cpp
@@ -1,44 +1,11 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <math.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <array>
-#define SIZE 146
-#define K 3
-#define DEBUG 0
-using namespace std;
-int multCount = 0;
-#include "src/Context.h"
-#include "src/SimpleOptimizer.h"
-#include "src/Solution.h"
+#include "include/TimeAlignment.h"
 
-typedef double numeric;/**< To easily change the used data type. */
+int main(){
+	string hyp_name = "materials/u4/test.dat";
+	string ref_name = "materials/u4/ref.dat";
+	vector<double> weights = {2.0,0.0,2.0};
 
-int main()
-{
-
-    ifstream file("material/u3/probe1.ascii.txt");/**< Define input file stream obj. */
-
-    if(!file) { exit(EXIT_FAILURE);}
-    string line;
-    vector<numeric> energies;
-
-    while(getline(file,line)){
-        energies.push_back(strtod(line.c_str(),nullptr));/**< Always use nullptr instead of NULL! */
-    }
-    SimpleOptimizer simp;
-    Solution sol;
-    Context<SimpleOptimizer> context(simp,sol,energies);
-    context.optimize();
-    //context.setEnergies(energies);
-    /*
-    context.setEnergies(energies);
-    vector<double>& test = context.getEnergies();
-    cout << test.first() << endl;
-    */
-
-    return 0;
+	TimeAlignment t(hyp_name, ref_name, weights);
+ 
+	return 0;
 }
